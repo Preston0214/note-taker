6ac7c3f8e02b86344a69703e765e75d0d8d398f6
Preston <prestonconley5@gmail.com>
2021-10-20 14:01:33 -0500

    further changes to server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-const PORT = process.env.PORT || 3001;
-
 const express = require("express");
 const path = require("path");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
